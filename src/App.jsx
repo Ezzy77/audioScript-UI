234cb3701b10c498661548d1dd73cb67329232de
Elisio <racabral11@icloud.com>
2024-05-13 17:12:05 +0100

    first commit, setup postCSS, tailwind and daisyUI and create NavBar and Footer components
    This commit sets up postCSS with a configuration file and an index.css file, and creates the NavBar, Home page and Footer components. It also includes package.json and package-lock.json updates for new dependencies and a README update to describe the application.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from "./components/NavBar.jsx";
+import HomePage from "./HomePage.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <body data-theme="cupcake">
+        <NavBar />
+        <HomePage />
+        <Footer />
+    </body>
+  )
+}
+export default App
---
+++ src/HomePage.jsx
@@ -0,0 +1,25 @@
+
+export default function HomePage() {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Hello there</h1>
+                    <p className="py-6">
+                        Welcome to AudioScript, where language
+                        barriers are a thing of the past. Unlock seamless
+                        communication and global connectivity with our
+                        innovative AI powered speech-to-text and document
+                        translation solutions.
+                    </p>
+                    <p className="py-6">
+                        From effortlessly transcribing meetings to translating documents
+                        with precision, empower your team to collaborate across languages
+                        and cultures like never before.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+}
